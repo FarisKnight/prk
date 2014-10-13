8cb44aaa24a660ba424417ceab03f642c68ca2e1
lloistborn <faisal_morensya@programmer.net>
2014-10-13 21:37:02 +0700

    add interface ISimilarity.cs

---
+++ ISimilarity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_Rabin_Karp_Application
+{
+    public interface ISimilarity
+    {
+        public float similarityMeasure(int lengthText, int lengthPatt, int stringFound);
+    }
+}
