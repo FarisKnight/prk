d2cf7d9cbaabdaad001b87ca97a2bc4340421db5
lloistborn <faisal_morensya@programmer.net>
2014-10-13 21:37:29 +0700

    add interface IVerify.cs

---
+++ IVerify.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_Rabin_Karp_Application
+{
+    public interface IVerify
+    {
+        public bool isMeetReq(string testDoc, string trainDoc);
+    }
+}
